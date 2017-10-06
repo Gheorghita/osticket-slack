fda0c75d2739cf18595ea26eb8d0aaa74356b6f0
Gheorghita BUTNARU <gheorghita.butnaru@gmail.com>
2017-10-06 12:21:36 +0300

    change plugin interface - add config for every department

--- slack/config.php
+++ slack/config.php
@@ -1,17 +1,26 @@
 <?php
 
 require_once INCLUDE_DIR . 'class.plugin.php';
+require_once INCLUDE_DIR . 'class.dept.php';
 
 class SlackPluginConfig extends PluginConfig {
     function getOptions() {        
-        return array(
-            'slack' => new SectionBreakField(array(
-                'label' => 'Slack notifier',
-            )),
-            'slack-webhook-url' => new TextboxField(array(
-                'label' => 'Webhook URL',
-                'configuration' => array('size'=>100, 'length'=>200),
-            )),			            
-        );
+	$depts = Dept::getDepartments();
+	$a = array();
+        foreach(array_keys($depts) as $deptId) {
+	   $dept = pathinfo($depts[$deptId]);
+	   $short_dept = $dept['basename'];
+           $b = array(
+                'slack'.$deptId => new SectionBreakField(array(
+                    'label' => 'Slack notifier '.$short_dept,
+                )),
+                'slack-webhook-url'.$deptId => new TextboxField(array(
+                    'label' => 'Webhook URL',
+                    'configuration' => array('size'=>100, 'length'=>200),
+                )),			            
+           );
+	   $a = array_merge($a, $b);
+	}
+	return $a;
     }	
 }
